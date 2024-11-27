0acef713ce6a3cd56dc2705723d4952b3f384de9
pxndacoder <91477140+pxndacoder@users.noreply.github.com>
2024-11-26 19:04:22 -0500

    Create Starter.java

---
+++ Starter.java
@@ -0,0 +1,6 @@
+public class Starter {
+    public static void main(String[] args) {
+        LoginScreen loginScreen = new LoginScreen();
+        loginScreen.launch(); // Start with the login screen
+    }
+}
